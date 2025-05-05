701d7e2147230c8aea756ba3abf7c3046aae879d
Junaid khan <junaid123.jk78@gmail.com>
2025-05-05 23:11:07 +0500

    Initial Commit

---
+++ SAMEABC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAMEABC
+//
+//  Created by Junaid Khan on 05/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
