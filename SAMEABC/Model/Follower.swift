e5b2738c34fe2da275d25a50d3a46ef06074edea
Junaid khan <junaid123.jk78@gmail.com>
2025-05-12 19:57:39 +0500

    make the EntityMaker and Created DTO and Domain models

--- SAMEABC/Model/Follower.swift
+++ SAMEABC/Model/Follower.swift
@@ -7,15 +7,22 @@
 
 
 import Foundation
-struct Follower: Codable {
-    var login: String
-    var avatarUrl: String
-}
 
-extension Follower {
-    var avatarURL: URL? {
-        URL(string: avatarUrl)
-    }
+struct Follower: Codable {
+    let login: String
+    let avatarUrl: URL?
 }
 
 extension Follower: Hashable { }
+
+struct FollowerDTO: Decodable {
+    let login: String
+    let avatarUrl: String
+}
+
+final class FollowerEntityMaker {
+    static func toFollower(from: FollowerDTO) -> Follower {
+        Follower(login: from.login,
+                 avatarUrl: URL(string: from.avatarUrl))
+    }
+}
